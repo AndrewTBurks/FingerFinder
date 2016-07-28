77fa194466695890da8970d38161ca85c1e8eb0e
AndrewTBurks <andrewtburks@gmail.com>
2016-07-28 14:24:19 -0600

    Panning graph with left mouse removed from tooltip

--- js/VISUALIZATION.js
+++ js/VISUALIZATION.js
@@ -2735,7 +2735,7 @@ function populateRunDropdown() {
 
 function setupTitleTooltips() {
 	var flowTooltip = "Left-Mouse ↻: Rotate Cylinder<br>Right-Mouse ↕: Pan Camera Vertically<br>Right-Mouse ↔: Pan Around Cylinder";
-	var forestTooltip = "<strong>Background:</strong><br>Mouse-Wheel: Zoom<br>Left-Mouse ↕↔: Pan<br>Right-Mouse: Reset Zoom<br><br><strong>Circle:</strong><br>Left-Mouse: Select/Deselect Viscous Finger<br>Hover: Show Viscous Finger Information";
+	var forestTooltip = "<strong>Background:</strong><br>Mouse-Wheel: Zoom<br>Right-Mouse: Reset Zoom<br><br><strong>Circle:</strong><br>Left-Mouse: Select/Deselect Viscous Finger<br>Hover: Show Viscous Finger Information";
 	var parallelTooltip = "<strong>Background:</strong><br>Left-Mouse:<br>&nbsp&nbsp&nbspDrag: Strum Select Lines<br>&nbsp&nbsp&nbspClick: Clear Selection<br><br><strong>Titles:</strong><br>Left-Mouse:<br>&nbsp&nbsp&nbsp↔: Rearrange Axes<br>&nbsp&nbsp&nbspDouble-Click: Flip Axis";
 
 
