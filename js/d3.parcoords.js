64d49b37f01742417dafc1efaf246f29a28fdb8a
csugiyama <c.sugiyama1@gmail.com>
2016-07-28 13:51:15 -0500

    animationTime smaller

--- js/d3.parcoords.js
+++ js/d3.parcoords.js
@@ -23,7 +23,7 @@ d3.parcoords = function(config) {
     showControlPoints: false,
     hideAxis : [],
     flipAxes: [],
-    animationTime: 1100, // How long it takes to flip the axis when you double click
+    animationTime: 500, // How long it takes to flip the axis when you double click
     rotateLabels: false
   };
 
